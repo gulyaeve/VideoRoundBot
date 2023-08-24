6ceb9b3146b52727fddd694d02a04406deb4c629
gulyaeve <gulyaev.e@icloud.com>
2023-08-24 20:13:45 +0300

    Added message before convertation

--- handlers/video_note.py
+++ handlers/video_note.py
@@ -8,6 +8,7 @@ router = Router()
 
 @router.message(F.video)
 async def video_to_note(message: types.Message):
+    await message.reply("В течение минуты ты получишь кружочек")
     file = message.video.file_id
     await bot.download(file, destination=f"temp/{file}.mp4", timeout=60)
     round_video = await make_round_video(f"temp/{file}.mp4")
